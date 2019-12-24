a5954c7fc4d3b35b56f014d7dd447eb4b364cdba
Zoe <zxenonwolf@gmail.com>
2019-12-24 15:08:21 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,20 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f930d8fc131cb1153501.js"
+    "url": "webpack-runtime-194f8e8de23c4d4224a2.js"
   },
   {
     "url": "commons-549c24a093027ad17843.js"
   },
   {
-    "url": "app-68674d15bcf7dd4bdb7b.js"
+    "url": "app-57ffc056da09125a7dde.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-6cc37212ef529ca81b4e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a782a23c42dc4adba1baa9f7a9a47d09"
+    "revision": "091266cccdd8c543ce8671158452b041"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "1052e6edf3f49e37c13611eb8c88e7ae"
+    "revision": "99345aaa2c0dad1b06bfd27f7d151f2e"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
@@ -75,7 +75,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/live-resume/app-68674d15bcf7dd4bdb7b.js`))) {
+  if (!resources || !(await caches.match(`/live-resume/app-57ffc056da09125a7dde.js`))) {
     return await fetch(event.request)
   }
 
